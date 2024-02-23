2c09b4673440e2d0e320bb1cc80fb1821f748e73
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-23 10:42:58 +0200

    task 0 - Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This Puppet manifest creates a file with content "I love Puppet", permissions '0744', owner 'www-data', and group "www-data".
+
+file { '/tmp/school':
+  ensure  => file,
+  content => "I love Puppet",
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
