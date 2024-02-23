d787f7c788eb7428f694efb116ef42f3c126e734
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-23 11:15:01 +0200

    lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # This Puppet manifest kills a process named 'killmenow'.
 
 exec { 'kill_process':
-  command     => 'pkill killmenow',
-  path        => ['/bin', '/usr/bin'],
-  onlyif      => 'pgrep killmenow',
+  command => 'pkill killmenow',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'pgrep killmenow',
 }
