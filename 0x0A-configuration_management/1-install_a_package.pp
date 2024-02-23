ca23b659ea0477acca66ebd03e9984551cf336d0
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-23 11:44:11 +0200

    Upper case 'F' in flsk + istalling Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# This Puppet manifest installs flask Version 2.1.0 from pip3.
+# This Puppet manifest installs flask Version 2.1.0 and Werkzeug Version 2.1.1 from pip3.
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+# Install Werkzeug as a requirement for Flask
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
