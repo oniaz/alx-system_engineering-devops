89df0315992e2c7d828c806507739060c5c00c84
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-27 09:49:49 +0200

    task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# This Puppet manifest modifies the configuration file to enable authentication without a password using the private key ~/.ssh/school.
+
+file_line { 'add_ssh_config_entry':
+  ensure  => present,
+  path => '~/.ssh/config',
+  line => "
+    Host *
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+  ",
+}
