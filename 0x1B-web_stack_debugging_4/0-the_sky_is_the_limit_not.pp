a94cee12f9dd7cd38b075a5bd42f05f68ddc5ec5
oniaz <32391049+oniaz@users.noreply.github.com>
2024-04-15 14:03:14 +0200

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# This Puppet manifest updates the ULIMIT value
+
+exec { 'update ulimit':
+  command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 65535\"/' /etc/default/nginx",
+  path    => ['/bin', '/usr/bin'],
+}
+
+-> exec { 'restart-nginx':
+  command  => 'service nginx restart',
+  provider => 'shell',
+}
