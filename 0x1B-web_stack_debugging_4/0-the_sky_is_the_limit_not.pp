e9cb7760fc7978f678d9c49136028b74008be58b
oniaz <32391049+oniaz@users.noreply.github.com>
2024-04-16 10:23:42 +0200

    niothing really

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # This Puppet manifest updates the ULIMIT value
 
-exec { 'update ulimit':
+exec { 'update-ulimit':
   command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 65535\"/' /etc/default/nginx",
   path    => ['/bin', '/usr/bin'],
 }
